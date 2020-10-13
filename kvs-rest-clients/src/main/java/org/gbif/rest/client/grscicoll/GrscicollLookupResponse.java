617e6e30000c93e88c3baf20f7e43c1d0f53dab9
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-10-13 16:41:48 +0200

    removed unnecessary grscicoll fields to avoid breaking this if the API changes

--- kvs-rest-clients/src/main/java/org/gbif/rest/client/grscicoll/GrscicollLookupResponse.java
+++ kvs-rest-clients/src/main/java/org/gbif/rest/client/grscicoll/GrscicollLookupResponse.java
@@ -1,17 +1,11 @@
 package org.gbif.rest.client.grscicoll;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
-import org.gbif.api.model.collections.Address;
 import org.gbif.api.model.collections.lookup.Match.MatchType;
 import org.gbif.api.model.collections.lookup.Match.Reason;
-import org.gbif.api.model.registry.Identifier;
 
 import lombok.Data;
 
@@ -20,37 +14,14 @@ import static org.gbif.api.model.collections.lookup.Match.Status;
 @Data
 public class GrscicollLookupResponse {
 
-  private Match<InstitutionResponse> institutionMatch;
-  private Match<CollectionResponse> collectionMatch;
+  private Match institutionMatch;
+  private Match collectionMatch;
 
   @Data
-  public static class Match<T> implements Serializable {
+  public static class Match implements Serializable {
     private MatchType matchType;
     private Status status;
     private Set<Reason> reasons;
-    private T entityMatched;
-  }
-
-  @Data
-  public static class InstitutionResponse implements Serializable {
     private UUID key;
-    private String code;
-    private String name;
-    private Map<String, String> alternativeCodes = new HashMap<>();
-    private List<Identifier> identifiers = new ArrayList<>();
-    private Address mailingAddress;
-    private Address address;
-  }
-
-  @Data
-  public static class CollectionResponse implements Serializable {
-    private UUID key;
-    private String code;
-    private String name;
-    private UUID institutionKey;
-    private Map<String, String> alternativeCodes = new HashMap<>();
-    private List<Identifier> identifiers = new ArrayList<>();
-    private Address mailingAddress;
-    private Address address;
   }
 }
