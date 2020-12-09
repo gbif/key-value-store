845679192eab7c816282da200b3ba845dd62479f
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-12-09 17:27:07 +0100

    restored reasons field

--- kvs-rest-clients/src/main/java/org/gbif/rest/client/grscicoll/GrscicollLookupResponse.java
+++ kvs-rest-clients/src/main/java/org/gbif/rest/client/grscicoll/GrscicollLookupResponse.java
@@ -1,9 +1,11 @@
 package org.gbif.rest.client.grscicoll;
 
 import java.io.Serializable;
+import java.util.Set;
 import java.util.UUID;
 
 import org.gbif.api.model.collections.lookup.Match.MatchType;
+import org.gbif.api.model.collections.lookup.Match.Reason;
 
 import lombok.Data;
 
@@ -19,6 +21,7 @@ public class GrscicollLookupResponse {
   public static class Match implements Serializable {
     private MatchType matchType;
     private Status status;
+    private Set<Reason> reasons;
     private EntityMatchedResponse entityMatched;
   }
 
